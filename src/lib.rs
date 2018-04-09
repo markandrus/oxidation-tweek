a671542784ba36dc3ca4ac9e66786e575e4fa1f6
Mark Andrus Roberts <markandrusroberts@gmail.com>
2018-04-09 16:57:25 -0700

    Remove cfg_if for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,40 @@
 #![feature(proc_macro, wasm_custom_section, wasm_import_module)]
 
-#[macro_use]
-extern crate cfg_if;
+#[cfg(target_arch = "wasm32")]
+extern crate wasm_bindgen;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
 
-cfg_if! {
-    if #[cfg(target_arch = "wasm32")] {
-        extern crate wasm_bindgen;
-        use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
-        #[wasm_bindgen]
-        extern {
-            #[wasm_bindgen(js_namespace = console)]
-            fn log(s: &str);
-        }
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    do_greet(name);
+}
 
-        #[wasm_bindgen]
-        pub fn greet(name: &str) {
-            do_greet(name);
-        }
-    } else {
-        extern crate libc;
-        use std::ffi::CStr;
-        use libc::c_char;
+#[cfg(not(target_arch = "wasm32"))]
+extern crate libc;
+#[cfg(not(target_arch = "wasm32"))]
+use std::ffi::CStr;
+#[cfg(not(target_arch = "wasm32"))]
+use libc::c_char;
 
-        fn log(str: &str) {
-            println!("{}", str);
-        }
+#[cfg(not(target_arch = "wasm32"))]
+fn log(str: &str) {
+    println!("{}", str);
+}
 
-        #[no_mangle]
-        pub extern "C" fn greet(name: *const c_char) {
-            let c_name = unsafe { CStr::from_ptr(name) };
-            do_greet(c_name.to_str().unwrap());
-        }
-    }
+#[cfg(not(target_arch = "wasm32"))]
+#[no_mangle]
+pub extern "C" fn greet(name: *const c_char) {
+    let c_name = unsafe { CStr::from_ptr(name) };
+    do_greet(c_name.to_str().unwrap());
 }
 
 fn do_greet(name: &str) {
